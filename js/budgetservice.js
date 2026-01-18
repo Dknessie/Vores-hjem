28964c4a68613f65c60d2af3eadf9830647d5ad0
Dknessie <dapela.88@gmail.com>
2026-01-18 09:39:37 +0100

    Update budgetservice.js

--- js/budgetservice.js
+++ js/budgetservice.js
@@ -1,1 +1,38 @@
+import { state } from "../app.js";
+import { collection, addDoc, getDocs, deleteDoc, doc } from "firebase/firestore";
 
+const COLLECTION_NAME = "budget_posts";
+
+/**
+ * Gemmer en ny budgetpost i Firestore
+ */
+export async function addBudgetPost(postData) {
+    try {
+        const docRef = await addDoc(collection(state.db, COLLECTION_NAME), postData);
+        return docRef.id;
+    } catch (e) {
+        console.error("Fejl ved tilføjelse af post: ", e);
+    }
+}
+
+/**
+ * Henter alle budgetposter
+ */
+export async function getBudgetPosts() {
+    const querySnapshot = await getDocs(collection(state.db, COLLECTION_NAME));
+    return querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+}
+
+/**
+ * Sletter en post
+ */
+export async function deleteBudgetPost(id) {
+    try {
+        await deleteDoc(doc(state.db, COLLECTION_NAME, id));
+    } catch (e) {
+        console.error("Fejl ved sletning: ", e);
+    }
+}
